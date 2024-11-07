80e328d230a09c23c81082c83ef40d2a59d0ad94
Samacharn Sankul <samacharn@aslsecurities.com>
2024-11-07 17:52:08 +0700

    Move setting loading logic to main.dart so they can be completely loaded before launching the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_template/src/app.dart';
+import 'package:get/get.dart';
+
+import 'src/controllers/setting_controller.dart';
+import 'src/services/setting_service.dart';
 
 void main() async {
+  Get.put(SettingService());
+  final SettingController settingController =
+      Get.put(SettingController(settingsService: Get.find()));
+  await settingController.loadSettings();
+
   runApp(App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,22 +2,17 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_template/src/controllers/setting_controller.dart';
 import 'package:flutter_application_template/src/routes/routes.dart';
-import 'package:flutter_application_template/src/services/setting_service.dart';
 import 'package:flutter_application_template/src/themes/themes.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 
 class App extends StatelessWidget {
-  final SettingService settingsService = Get.put(SettingService());
-  final SettingController settingController =
-      Get.put(SettingController(settingsService: Get.find()));
-
-  App({super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    settingController.loadSettings();
+    final SettingController settingController = Get.find();
     return Obx(() {
       return GetMaterialApp(
         theme: FlexColorScheme.light(colorScheme: lightColorScheme).toTheme,
