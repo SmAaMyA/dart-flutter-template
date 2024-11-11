8c7a79a19b7f12176669a9b4b479767000822adc
Samacharn Sankul <samacharn@aslsecurities.com>
2024-11-11 15:21:49 +0700

    Add room image, number of unread message and last message to be shown in chat room list page.

--- lib/src/view/pages/shared/chat_room_list_page.dart
+++ lib/src/view/pages/shared/chat_room_list_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_template/src/view/pages/shared/chat_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'chat_page.dart';
 
 class ChatRoomListPage extends StatelessWidget {
   const ChatRoomListPage({super.key});
@@ -8,11 +8,41 @@ class ChatRoomListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final chatRooms = [
-      {'name': 'ห้องรวม'},
-      {'name': 'อาจารย์หวาน'},
-      {'name': 'เจฟ'},
-      {'name': 'ติ๊ด'},
-      {'name': 'มะปราง'},
+      {
+        'name': 'ห้องรวม',
+        'unread': 5,
+        'image': 'assets/images/room1.png',
+        'latestMessage': 'Hello everyone!'
+      },
+      {
+        'name': 'อาจารย์หวาน',
+        'unread': 2,
+        'image': 'assets/images/room2.png',
+        'latestMessage': 'Please submit your assignments.'
+      },
+      {
+        'name': 'เจฟ',
+        'unread': 0,
+        'image': 'assets/images/room3.png',
+        'latestMessage': 'See you tomorrow.'
+      },
+      {
+        'name': 'ติ๊ด',
+        'unread': 3,
+        'image': 'assets/images/room4.png',
+        'latestMessage': 'Can we meet at 5 PM?'
+      },
+      {
+        'name': 'มะปราง',
+        'unread': 1,
+        'image': 'assets/images/room5.png',
+        'latestMessage': 'I will be late.'
+      },
+      {
+        'name': 'ห้องใหม่',
+        'unread': 0,
+        'latestMessage': 'Welcome to the new room!'
+      },
     ];
 
     return Scaffold(
@@ -23,14 +53,46 @@ class ChatRoomListPage extends StatelessWidget {
         itemCount: chatRooms.length,
         itemBuilder: (context, index) {
           final chatRoom = chatRooms[index];
+          final image = chatRoom['image'];
+          final name = chatRoom['name'] as String;
+          final unread = chatRoom['unread'] as int;
+          final latestMessage = chatRoom['latestMessage'] as String;
+
           return ListTile(
-            title: Text(chatRoom['name']!),
+            leading: CircleAvatar(
+              backgroundImage:
+                  image != null ? AssetImage(image as String) : null,
+              child: image == null
+                  ? Text(
+                      name[0],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                      ),
+                    )
+                  : null,
+              backgroundColor: image == null ? Colors.blue : null,
+            ),
+            title: Text(name),
+            subtitle: Text(latestMessage),
+            trailing: unread > 0
+                ? CircleAvatar(
+                    radius: 12.0,
+                    backgroundColor: Colors.red,
+                    child: Text(
+                      unread.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  )
+                : null,
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) =>
-                      ChatPage(chatRoomName: chatRoom['name']!),
+                  builder: (context) => ChatPage(chatRoomName: name),
                 ),
               );
             },
