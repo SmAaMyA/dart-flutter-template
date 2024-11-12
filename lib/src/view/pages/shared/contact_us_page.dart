15bcf055058b15b298a2fb2b6f5662246ff2be7a
Samacharn Sankul <samacharn@aslsecurities.com>
2024-11-12 12:11:25 +0700

    Change random color to primary color. Add primary color to header.

--- lib/src/view/pages/shared/contact_us_page.dart
+++ lib/src/view/pages/shared/contact_us_page.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_template/src/controllers/branch_controller.dart';
 import 'package:flutter_application_template/src/model/branch_model.dart';
@@ -18,7 +17,18 @@ class ContactUsPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.contactUs),
+        title: Text(
+          AppLocalizations.of(context)!.contactUs,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        elevation: 4.0,
+        centerTitle: true,
+        iconTheme:
+            IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -40,8 +50,15 @@ class ContactUsPage extends StatelessWidget {
                 child: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    GestureDetector(
-                      onTap: () async {
+                    IconButton(
+                      icon: SvgPicture.asset(
+                        'assets/images/logo/line_logo.svg',
+                        fit: BoxFit.cover,
+                        width: 48,
+                        height: 48,
+                      ),
+                      tooltip: AppLocalizations.of(context)!.lineMenuTooltip,
+                      onPressed: () async {
                         const String url = 'https://line.me/ti/p/~@aslonnet';
                         final Uri uri = Uri.parse(url);
                         if (await canLaunchUrl(uri)) {
@@ -52,22 +69,18 @@ class ContactUsPage extends StatelessWidget {
                               url;
                         }
                       },
-                      child: Tooltip(
-                        message: AppLocalizations.of(context)!.lineMenuTooltip,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.asset(
-                            'assets/images/logo/line_logo.svg',
-                            fit: BoxFit.cover,
-                            width: 48,
-                            height: 48,
-                          ),
-                        ),
-                      ),
                     ),
                     SizedBox(width: 16.0),
-                    GestureDetector(
-                      onTap: () async {
+                    IconButton(
+                      icon: SvgPicture.asset(
+                        'assets/images/logo/facebook_logo.svg',
+                        fit: BoxFit.cover,
+                        width: 48,
+                        height: 48,
+                      ),
+                      tooltip:
+                          AppLocalizations.of(context)!.facebookMenuTooltip,
+                      onPressed: () async {
                         const String url =
                             'https://www.facebook.com/ASLSecurities';
                         final Uri uri = Uri.parse(url);
@@ -79,23 +92,17 @@ class ContactUsPage extends StatelessWidget {
                               url;
                         }
                       },
-                      child: Tooltip(
-                        message:
-                            AppLocalizations.of(context)!.facebookMenuTooltip,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.asset(
-                            'assets/images/logo/facebook_logo.svg',
-                            fit: BoxFit.cover,
-                            width: 48,
-                            height: 48,
-                          ),
-                        ),
-                      ),
                     ),
                     SizedBox(width: 16.0),
-                    GestureDetector(
-                      onTap: () async {
+                    IconButton(
+                      icon: SvgPicture.asset(
+                        'assets/images/logo/youtube_logo.svg',
+                        fit: BoxFit.cover,
+                        width: 48,
+                        height: 48,
+                      ),
+                      tooltip: AppLocalizations.of(context)!.youtubeMenuTooltip,
+                      onPressed: () async {
                         const String url =
                             'https://www.youtube.com/@aslsecuritiesco.ltd.4579';
                         final Uri uri = Uri.parse(url);
@@ -107,23 +114,18 @@ class ContactUsPage extends StatelessWidget {
                               url;
                         }
                       },
-                      child: Tooltip(
-                        message:
-                            AppLocalizations.of(context)!.youtubeMenuTooltip,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.asset(
-                            'assets/images/logo/youtube_logo.svg',
-                            fit: BoxFit.cover,
-                            width: 48,
-                            height: 48,
-                          ),
-                        ),
-                      ),
                     ),
                     SizedBox(width: 16.0),
-                    GestureDetector(
-                      onTap: () async {
+                    IconButton(
+                      icon: SvgPicture.asset(
+                        'assets/images/logo/tiktok_logo.svg',
+                        fit: BoxFit.cover,
+                        width: 48,
+                        height: 48,
+                      ),
+                      tooltip:
+                          AppLocalizations.of(context)!.facebookMenuTooltip,
+                      onPressed: () async {
                         const String url =
                             'https://www.tiktok.com/@aslsecurities';
                         final Uri uri = Uri.parse(url);
@@ -135,19 +137,6 @@ class ContactUsPage extends StatelessWidget {
                               url;
                         }
                       },
-                      child: Tooltip(
-                        message:
-                            AppLocalizations.of(context)!.tiktokMenuTooltip,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.asset(
-                            'assets/images/logo/tiktok_logo.svg',
-                            fit: BoxFit.cover,
-                            width: 48,
-                            height: 48,
-                          ),
-                        ),
-                      ),
                     ),
                   ],
                 ),
@@ -193,181 +182,179 @@ class ContactUsPage extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _buildBranchInfo(BuildContext context, String branchName, String address,
-    String phone, String email, String workingHours, LatLng location) {
-  final random = Random();
-  final randomColor = Color.fromARGB(
-    255,
-    random.nextInt(256),
-    random.nextInt(256),
-    random.nextInt(256),
-  );
-
-  return Stack(
-    children: [
-      Card(
-        color: Theme.of(context).cardColor,
-        margin: EdgeInsets.symmetric(vertical: 16.0),
-        elevation: 4.0,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 32.0), // Space for the oval
-              Row(
-                children: [
-                  Icon(Icons.location_on,
-                      color: Theme.of(context).colorScheme.inverseSurface),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: InkWell(
-                      onTap: () async {
-                        final Uri uri = Uri.parse(
-                            'https://www.google.com/maps/search/?api=1&query=${Uri.encodeComponent(address)}');
-                        if (await canLaunchUrl(uri)) {
-                          await launchUrl(uri);
-                        } else {
-                          throw AppLocalizations.of(context)!.failLaunchUrl +
-                              ' ' +
-                              uri.toString();
-                        }
-                      },
-                      child: Text(
-                        address,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Theme.of(context).colorScheme.primary,
+  Widget _buildBranchInfo(
+      BuildContext context,
+      String branchName,
+      String address,
+      String phone,
+      String email,
+      String workingHours,
+      LatLng location) {
+    return Stack(
+      children: [
+        Card(
+          color: Theme.of(context).cardColor,
+          margin: EdgeInsets.symmetric(vertical: 16.0),
+          elevation: 4.0,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 32.0),
+                Row(
+                  children: [
+                    Icon(Icons.location_on,
+                        color: Theme.of(context).colorScheme.inverseSurface),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () async {
+                          final Uri uri = Uri.parse(
+                              'https://www.google.com/maps/search/?api=1&query=${Uri.encodeComponent(address)}');
+                          if (await canLaunchUrl(uri)) {
+                            await launchUrl(uri);
+                          } else {
+                            throw AppLocalizations.of(context)!.failLaunchUrl +
+                                ' ' +
+                                uri.toString();
+                          }
+                        },
+                        child: Text(
+                          address,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10.0),
-              Row(
-                children: [
-                  Icon(Icons.phone,
-                      color: Theme.of(context).colorScheme.inverseSurface),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: Wrap(
-                      children: phone.split(',').expand((phoneNumber) {
-                        return [
-                          InkWell(
-                            onTap: () async {
-                              final Uri uri =
-                                  Uri(scheme: 'tel', path: phoneNumber.trim());
-                              if (await canLaunchUrl(uri)) {
-                                await launchUrl(uri);
-                              } else {
-                                throw AppLocalizations.of(context)!
-                                        .failLaunchUrl +
-                                    ' ' +
-                                    uri.toString();
-                              }
-                            },
-                            child: Text(
-                              phoneNumber.trim(),
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Theme.of(context).colorScheme.primary,
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  children: [
+                    Icon(Icons.phone,
+                        color: Theme.of(context).colorScheme.inverseSurface),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: Wrap(
+                        children: phone.split(',').expand((phoneNumber) {
+                          return [
+                            InkWell(
+                              onTap: () async {
+                                final Uri uri = Uri(
+                                    scheme: 'tel', path: phoneNumber.trim());
+                                if (await canLaunchUrl(uri)) {
+                                  await launchUrl(uri);
+                                } else {
+                                  throw AppLocalizations.of(context)!
+                                          .failLaunchUrl +
+                                      ' ' +
+                                      uri.toString();
+                                }
+                              },
+                              child: Text(
+                                phoneNumber.trim(),
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
                               ),
                             ),
-                          ),
-                          Text(
-                            ', ',
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                        ];
-                      }).toList()
-                        ..removeLast(), // Remove the last comma
+                            Text(
+                              ', ',
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ];
+                        }).toList()
+                          ..removeLast(),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10.0),
-              Row(
-                children: [
-                  Icon(Icons.email,
-                      color: Theme.of(context).colorScheme.inverseSurface),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: InkWell(
-                      onTap: () async {
-                        final Uri uri = Uri(scheme: 'mailto', path: email);
-                        if (await canLaunchUrl(uri)) {
-                          await launchUrl(uri);
-                        } else {
-                          throw AppLocalizations.of(context)!.failLaunchUrl +
-                              ' ' +
-                              uri.toString();
-                        }
-                      },
-                      child: Text(
-                        email,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Theme.of(context).colorScheme.primary,
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  children: [
+                    Icon(Icons.email,
+                        color: Theme.of(context).colorScheme.inverseSurface),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () async {
+                          final Uri uri = Uri(scheme: 'mailto', path: email);
+                          if (await canLaunchUrl(uri)) {
+                            await launchUrl(uri);
+                          } else {
+                            throw AppLocalizations.of(context)!.failLaunchUrl +
+                                ' ' +
+                                uri.toString();
+                          }
+                        },
+                        child: Text(
+                          email,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10.0),
-              Row(
-                children: [
-                  Icon(Icons.access_time,
-                      color: Theme.of(context).colorScheme.inverseSurface),
-                  SizedBox(width: 8.0),
-                  Text(
-                    workingHours,
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10.0),
-              SizedBox(
-                height: 200.0,
-                child: GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                    target: location,
-                    zoom: 14.0,
-                  ),
-                  markers: {
-                    Marker(
-                      markerId: MarkerId(branchName),
-                      position: location,
-                    ),
-                  },
+                  ],
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      Positioned(
-        top: 0,
-        left: 16.0,
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          decoration: BoxDecoration(
-            color: randomColor,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          child: Text(
-            branchName,
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+                SizedBox(height: 10.0),
+                Row(
+                  children: [
+                    Icon(Icons.access_time,
+                        color: Theme.of(context).colorScheme.inverseSurface),
+                    SizedBox(width: 8.0),
+                    Text(
+                      workingHours,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                SizedBox(
+                  height: 200.0,
+                  child: GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: location,
+                      zoom: 14.0,
+                    ),
+                    markers: {
+                      Marker(
+                        markerId: MarkerId(branchName),
+                        position: location,
+                      ),
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-      ),
-    ],
-  );
+        Positioned(
+          top: 0,
+          left: 16.0,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Text(
+              branchName,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
